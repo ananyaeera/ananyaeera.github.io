2faa5f580edd086651527cf0eddf159c266cc5e7
ananyaeera <aeeraveni@gmail.com>
2026-04-26 05:37:23 +0000

    Deploying to gh-pages from @ ananyaeera/ananyaeera.github.io@af16ddd9878191ca0287984f04c7ae1edd69b56e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -481,8 +481,8 @@ ninja.data = [{
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
+            },},{id: "projects-jae-brooke-ananya-2026",
+          title: 'jae, brooke, ananya 2026',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
