d5d990e46d955eadb6f3b3ee7acfe34855c416fe
ananyaeera <aeeraveni@gmail.com>
2026-04-30 20:29:58 +0000

    Deploying to gh-pages from @ ananyaeera/ananyaeera.github.io@ced163bde8cc8b8066639187595bbb0ff7e424e0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -427,9 +427,9 @@ ninja.data = [{
           description: "tribeca, manhattan | november 2025",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-summering-in-london",
+          title: 'summering in london',
+          description: "london, uk|may 2025",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
