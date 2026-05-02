a763cd9193c98dcad5a2f6f856c69fe33af7c697
ananyaeera <aeeraveni@gmail.com>
2026-05-02 16:18:48 +0000

    Deploying to gh-pages from @ ananyaeera/ananyaeera.github.io@760289b31cdf1c406dc96a4efbb1ceab96ce90bf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "How lucky I am to experience life with my best friends.",
+          description: "How lucky I am to experience life with my best friends. See my adventures below :)",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
