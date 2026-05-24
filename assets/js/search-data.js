99fa9613b9285ebde8663359e01fe9462453f782
ananyaeera <aeeraveni@gmail.com>
2026-05-24 15:16:08 +0000

    Deploying to gh-pages from @ ananyaeera/ananyaeera.github.io@07bd15d95b7d01d61b612f8a0345a31ec0e16e02 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -445,9 +445,9 @@ ninja.data = [{
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
+            },},{id: "projects-trio-reunited",
+          title: 'trio reunited',
+          description: "chicago | march 2026",
           section: "Projects",handler: () => {
               window.location.href = "/projects/8_project/";
             },},{id: "projects-project-9",
